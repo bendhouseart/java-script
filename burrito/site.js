0d21c1e364dce27f954944355fedb9858e41e7bb
Anthony Galassi <anthony.e.galassi@gmail.com>
2017-07-09 22:14:38 -0700

    made worse, showlist with hidden instead

--- burrito/site.js
+++ burrito/site.js
@@ -1,158 +1,253 @@
 $('.ui.checkbox').checkbox();
 console.log("If you're reading this I hope you have a wonderful day");
 var tortilla = {
-	'white': true, 
-	'wheat': false, 
-	'spinach': false, 
-	'corn': false};
+    'white tortilla': true,
+    'wheat tortilla': false,
+    'spinach tortilla': false,
+    'corn tortilla': false
+};
 var meat = {
-	'carnitas': false,
-	'chicken': false, 
-	'sofritas': false};
+    'carnitas': false,
+    'chicken': false,
+    'sofritas': false
+};
 var standards = {
-	'beans': true, 
-	'cheese': true, 
-	'salsa': true, 
-	'sour cream': true};
+    'beans': true,
+    'cheese': true,
+    'salsa': true,
+    'sour cream': true
+};
 var extras = {
-	'guacamole': false,
-   	'scrambled-egg': false, 
-	'potatoes': false,
-	'sun-dried-tomatoes': false,
-	'olives': false,
-	'sauteed-mushrooms': false,
-	'sauteed-onions': false,
-	'jalapenos': false};
+    'guacamole': false,
+    'scrambled-egg': false,
+    'potatoes': false,
+    'sun-dried-tomatoes': false,
+    'olives': false,
+    'sauteed-mushrooms': false,
+    'sauteed-onions': false,
+    'jalapenos': false
+};
 var details = {
-	'name': '',
-	'ccNumber': 0000000000000000,
-	'cvv': 123,
-	'zip': 12345,
-	'delivery': false,
-	'terms': false,
-	'total': 6.00
+    'name': '',
+    'credit-card': 0000000000000000,
+    'cvv': 123,
+    'zip': 12345,
+    'delivery': false,
+    'terms': false,
+    'total': 6.00,
+    'fail': true
 };
 var burrito = {};
-var orderList= [];
+var orderList = [];
 /* This function will listen to the check boxes and various text
  * input on the page and update the burrito order as changes are made. */
-$(document).ready(function() {
-	 
-	 $('input[type=radio][name=tortilla]').on('change',function(event) {
-		 	var select;
-		 	select = $(this).attr('value');				
-		 	event.preventDefault();
-			Object.keys(tortilla).forEach(function(currentKey) {
-				tortilla[currentKey] = false;});
-	 		tortilla[select] = !tortilla[select];
-			console.log(tortilla);
-	});
+$(document).ready(function () {
+
+    $('input[type=radio][name=tortilla]').on('change', function (event) {
+        var select;
+        select = $(this).attr('value');
+        event.preventDefault();
+        Object.keys(tortilla).forEach(function (currentKey) {
+            tortilla[currentKey] = false;
+        });
+        tortilla[select] = !tortilla[select];
+        console.log(tortilla);
+		// updating the Order Total in window
+		orderSummary(tortilla);
+    });
 });
 
-		
+
 /* This function monitors meat for the burrito and makes changes
  * as checkboxes are selected and unselected. */
-$(document).ready(function() {
-	$('input[type=checkbox][name=meat]').on('change', function(event) {
-		var select;
-		select = $(this).attr('value');
-		meat[select] = !meat[select];
-		console.log(meat);
-	});
+$(document).ready(function () {
+    $('input[type=checkbox][name=meat]').on('change', function (event) {
+        var select;
+        select = $(this).attr('value');
+        meat[select] = !meat[select];
+        console.log(meat);
+		// updating the Order Total in window
+		orderSummary(meat);
+    });
 });
 
 
 /* This function monitors selections for standard toppings/ingredients
  * and makes changes as checkboxes are selected/deselected. */
-$(document).ready(function() {
-	$('input[type=checkbox][name=included-ingredients]').on('change', function(event) {
-		var select;
-		select = $(this).attr('value');
-		standards[select] = !standards[select];
-		console.log(standards);
-	});
+$(document).ready(function () {
+    $('input[type=checkbox][name=included-ingredients]').on('change', function (event) {
+        var select;
+        select = $(this).attr('value');
+        standards[select] = !standards[select];
+        console.log(standards);
+		// updating the order total in window
+		orderSummary(standards);
+    });
 });
 
 /* This function monitos selections for extra toppings and modifies
  * the extra dictionary as checkboxes are selected/deselected. */
-$(document).ready(function() {
-	$('input[type=checkbox][name=extra-ingredients]').on('change', function(event) {
-			var select;
-			select = $(this).attr('value');
-			extras[select] = !extras[select];
-			console.log(extras);
-	});
+$(document).ready(function () {
+    $('input[type=checkbox][name=extra-ingredients]').on('change', function (event) {
+        var select;
+        select = $(this).attr('value');
+        extras[select] = !extras[select];
+        console.log(extras);
+		// updating the order total in window
+		orderSummary(extras);
+    });
 });
 
+$(document).ready(function () {
+    $('button[type=submit]').hover(function (event) {
+        event.preventDefault();
+        if ($.inArray(false, proceed) >= 0) {
+            $('button[type=submit]').attr("disabled", true);
+            console.log("this should only fire once");
+        }
+        else if ($.inArray(false, proceed) == -1) {
+            $('button[type=submit]').removeAttr("disabled");
+            console.log("button on conditional firing.");
+        }
+    })
+});
 /* This function makes a burrito object and adds it to our order list
  */
-$(document).ready(function() {
-	$('button[type=submit]').on('click', function(event) {
-		event.preventDefault();
-		var	burrito = { 
-			'tortilla': tortilla, 
-			'meat': meat, 
-			'standards': standards,
-		    'extras' : extras,
-			'details': details
-		};
-		summation(extras, details);	
-		console.log(burrito);
-	});
+$(document).ready(function () {
+
+    $('button[type=submit]').on('click', function (event) {
+        event.preventDefault();
+        var burrito = {
+            'tortilla': tortilla,
+            'meat': meat,
+            'standards': standards,
+            'extras': extras,
+            'details': details
+        };
+        summation(extras, details);
+        console.log(burrito);
+    });
+    console.log(proceed);
+    console.log(!proceed.includes(false));
+
 });
 
 /* This function will add up the total cost of the burrito plus extra
  * ingredients and delivery */
 function summation(extras, details) {
-	var sum = 6.00;
-	Object.keys(extras).forEach(function(currentKey) {
-		if (extras[currentKey] === true) {
-			sum += 0.50;
-		}
-	});
-	if (details['delivery'] === true) {
-		sum += 5.00;
-	}
-	details['total'] = sum;
-};
-
+    var sum = 6.00;
+    Object.keys(extras).forEach(function (currentKey) {
+        if (extras[currentKey] === true) {
+            sum += 0.50;
+        }
+    });
+    if (details['delivery'] === true) {
+        sum += 5.00;
+    }
+    details['total'] = sum;
+}
 /* Delivery monitor */
-$(document).ready(function() {
-	$('input[type=radio][name=delivery').on('change', function() {
-		details['delivery'] = !details['delivery'];
-		console.log(details);
-		});
+$(document).ready(function () {
+    $('input[type=radio][name=delivery]').on('change', function () {
+        details['delivery'] = !details['delivery'];
+        console.log(details);
+    });
 });
 
 /* Agreement terms monitor */
-$(document).ready(function() {
-	$('input[type=checkbox][name=terms]').on('change', function() {
-		details['terms'] = !details['terms'];
-		console.log(details);
-	});
+$(document).ready(function () {
+    $('input[type=checkbox][name=terms]').on('change', function () {
+        details['terms'] = !details['terms'];
+        console.log(details);
+    });
 });
-	
+
 
 /* Name, credit card number, cvv, and zip code monitor. */
-$(document).ready(function() {
-	var whichField;
-	$('input[type=text]').on('change', function() {
-		whichField = $(this).attr('name');
-		console.log(whichField);
-		if (whichField === 'zip') {
-			console.log('ping');
-			console.log($(this).attr('val')); 
-		}
+var proceed = [false, false, false, false];
+/* proceed will increment to 4 once all field entries
+ have been verified */
+$(document).ready(function () {
+    var whichField;
+    $('input[type=text]').on('change', function () {
+        whichField = $(this).attr('name');
+        if (whichField === 'zip') {
+            var contains5D = $(this).val().replace(/\D/g, '');
+            if (/^\d{5}$/.test($(this).val())) {
+                console.log($(this).val());
+                details['zip'] = contains5D;
+                proceed[0] = true;
+                $(this).siblings().removeClass('error');
+            } else {
+                $(this).siblings().addClass('error');
+                console.log('you fail zip');
+                console.log($(this).siblings()[0]);
+                proceed[0] = false;
+            }
+        }
+        if (whichField === 'name') {
+            if (/^[\D\s]+$/.test($(this).val())) {
+                var containsName = $(this).val();
+                console.log($(this).val());
+                details['name'] = containsName;
+                proceed[1] = true;
+                $(this).siblings().removeClass('error');
+            } else {
+                $(this).siblings().addClass('error');
+                console.log('you fail name');
+                proceed[1] = false;
+            }
+        }
+        if (whichField === 'credit-card') {
+            var containsFD = $(this).val().replace(/\D/g, '');
+            if (/^\d{16}$/.test(containsFD)) {
+                console.log($(this).val());
+				console.log(containsFD);
+                details['credit-card'] = containsFD;
+                proceed[2] = true;
+                $(this).siblings().removeClass('error');
+            } else {
+                $(this).siblings().addClass('error');
+                console.log('you fail credit-card');
+                proceed[2] = false;
+            }
+        }
+        else {
 
+        }
+        if (whichField === 'cvv') {
+            var contains3D = $(this).val().replace(/\D/g, '');
+            console.log(contains3D);
+            if (/^\d{3}$/.test(contains3D)) {
+                console.log(contains3D);
+                details['cvv'] = contains3D;
+                proceed[3] = true;
+                $(this).siblings().removeClass('error');
+            } else {
+                $(this).siblings().addClass('error');
+                console.log('you fail cvv');
+                proceed[3] = false;
+            }
+
+        } else {
+            console.log('oh noes');
+        }
+        console.log(proceed);
+        if ($.inArray(false, proceed) == -1) {
+            $('button[type=submit]').removeAttr("disabled");
+        }
+    });
+        });
+
+/* This function adds the current contents of the burrito to the
+ abbreviated menu on the top right of the page. It simply reads
+ through the tortilla, meat, standards, and extras dictionaries
+ and then if those elements are true adds them to the html. */
+function orderSummary(burritoSubset) {
+	var values = $.map(burritoSubset, function(value, key) {
+
+	 
+		return key;					 
 	});
-	console.log(whichField);
-	/* now that we know which field has been entered we 
-	 * will apply a filter/validation to the input to each 
-	 * field in turn. */
-	});
-	
-
-/* This function checks the name and credit card info 
- * for our user and is called once the submit button is pressed */
-
-
+	console.log(values);
+}
--- weather.js
+++ weather.js
@@ -13,7 +13,7 @@ var i = 1;
 // zip code and then update the global variables to reflect said
 // input.
 function getLoc() {
-//	event.preventDefault();
+//	event.preventDefault();		//console.log(data)
 	var userInput = $("#loc").val();
 	var	break_condition = true;
 //	var weatherLoc = 97123;
@@ -38,21 +38,78 @@ function getLoc() {
 		break_condition = false;}
 	}
 	package[zip_or_city_state] = weatherLoc;
+	var response;
+	////////////////////////////////////////////////////////////////
+	//all calls for weather conditions and changes to the html page
+	//as a result will be done from within the success portion of the 
+	//ajax snippet below.
 	////////////////////////////////////////////////////////////////
 	$.ajax({
 		url: "http://api.openweathermap.org/data/2.5/weather",
 		data: package,
 		dataType: "jsonp",
 		success: function (data) {
-			console.log(data)
+			response = data;
+			console.log(data);
+			console.log(data.main.name);
+			$("#location").html(data.name);
+			$("#temperature").html(data.main.temp - 273.15);
+			$("#humidity").html(data.main.humidity);
+			$("#windVector").html(data.wind.deg);
+			$("#windSpeed").html(data.wind.speed);
+			$("#cloudCover").html(data.weather[0].description);
+			$("#conditions").html(data.weather[0].main);
 		}
 	});
-	return package;
+	return response;
 };         	
 
-function getWeather () {
+
+var icons = {
+	"01d": "http://openweathermap.org/img/w/01d.png",
+	"01n": "http://openweathermap.org/img/w/01n.png",
+	"02d": "http://openweathermap.org/img/w/02d.png",
+	"02n": "http://openweathermap.org/img/w/02n.png",
+	"03d": "http://openweathermap.org/img/w/03d.png",
+	"03n": "http://openweathermap.org/img/w/03n.png",
+	"04d": "http://openweathermap.org/img/w/04d.png",
+	"04n": "http://openweathermap.org/img/w/04n.png",
+	"09d": "http://openweathermap.org/img/w/09d.png",
+	"09n": "http://openweathermap.org/img/w/09n.png",
+	"10d": "http://openweathermap.org/img/w/10d.png",
+	"10n": "http://openweathermap.org/img/w/10n.png",
+	"11d": "http://openweathermap.org/img/w/11d.png",
+	"11n": "http://openweathermap.org/img/w/11n.png",
+	"13d": "http://openweathermap.org/img/w/13d.png",
+	"13n": "http://openweathermap.org/img/w/13n.png",
+	"50d": "http://openweathermap.org/img/w/50d.png",
+	"50n": "http://openweathermap.org/img/w/50n.png"
 };
 
+var backGrounds = {
+	"01d": "weather_images/clear_sky_d.jpg",
+	"01n": "weather_images/clear_sky_n.jpg",
+	"02d": "weather_images/few_clouds_d.jpg",
+	"02n": "weather_images/few_clouds_n.jpg",
+	"03d": "weather_images/scattered_clouds_d.jpg",
+	"03n": "weather_images/scattered_clouds_n.jpg",
+	"04d": "weather_images/broken_clouds_d.jpg",
+	"04n": "weather_images/broken_clouds_n.jpg",
+	"09d": "weather_images/shower_rain_d.jpg",
+	"09n": "weather_images/shower_rain_n.jpg",
+	"10d": "weather_images/rain_d.jpg",
+	"10d": "weather_images/rain_n.jpg",
+	"11d": "weather_images/thunderstorm_d.jpg",
+	"11n": "weather_images/thunderstorm_n.jpg",
+	"13d": "weather_images/snow_d.jpg",
+	"13n": "weather_images/snowe_n.jpg",
+	"50d": "weather_images/mist_d.jpg",
+	"50n": "weather_images/mist_n.jpg"
+};
+
+
+// here we call the function getLoc on button press of the submit button
 $("#loc_button").click(getLoc);
+
 
 
